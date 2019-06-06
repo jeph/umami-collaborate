67a57ba858d42148fc8285238b2bdc8c40c399eb
Jeph <jephliu@jeph.io>
2019-06-05 20:58:24 -0400

    Hit the right server

--- frontend/src/utils/SocketConnect.js
+++ frontend/src/utils/SocketConnect.js
@@ -3,9 +3,7 @@ import io from 'socket.io-client'
 export default function () {
   if (window.location.href.includes('localhost') || window.location.href.includes('127.0.0.1')) {
     return io.connect('127.0.0.1:3000')
-  } else if (window.location.href.includes('https://letsdraw.me')) {
-    return io.connect('https://server.letsdraw.me')
   } else {
-    return io.connect('https://devserver.letsdraw.me')
+    return io.connect('https://umami-collaborate.herokuapp.com')
   }
 }
